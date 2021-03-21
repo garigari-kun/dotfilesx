80f76580f2b7dc3cfcc7693f3e1493e5b12295dd
garigari-kun <keisuke.cs@gmail.com>
2021-03-21 06:11:22 +0000

    ready for setup scripts

---
+++ setup_tmux.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# after run
+# source ~/.tmux.conf
+# Press prefix + I to install plugins
---
+++ setup_zsh.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+# $ which zsh
+# $ sudo vi /etc/shells write the result of which zsh
+# $ chsh -s $(which zsh)
+# $ curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+# $ source ./zshrc
