3ec021c9f641a12e921f489d79bd6ad75f6bb940
garigari-kun <keisuke.cs@gmail.com>
2021-03-21 07:32:47 +0000

    fix comments and created macos

---
+++ macos/setup_for_mac.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew update
+brew doctor
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+brew install git
+brew unlink vim && brew link vim
+brew install vim
+brew install peco
+brew install tmux
+brew install go
+brew install python
+brew install docker
+brew install kubernetes-cli
+brew install terraform
+brew install httpie
+brew install tig
+brew install ag
+brew install direnv
+brew install reattach-to-user-namespace
+## GUI
+brew install docker
+brew install google-chrome
+brew install iterm2
+brew install alfred
+brew install sequel-pro
+brew install slack
+brew install virtualbox
+brew install visual-studio-code
+brew install spectacle
+brew install 1password
+brew install spotify
+brew install alacritty
